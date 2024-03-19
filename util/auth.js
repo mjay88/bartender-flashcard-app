fddcc12ce2457034d503d4aa3edc7fcf9c5c1f8b
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-03-19 16:38:00 -0500

    add axios, createUser function in auth.js

--- util/auth.js
+++ util/auth.js
@@ -1,7 +1,13 @@
 import axios from "axios";
+import { GOOGLE_API_KEY } from "@env";
 
-function createUser() {
-	axios.post(
-		"https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]"
+export async function createUser(email, password) {
+	const response = await axios.post(
+		`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${GOOGLE_API_KEY}`,
+		{
+			email: email,
+			password: password,
+			returnSecureToken,
+		}
 	);
 }
